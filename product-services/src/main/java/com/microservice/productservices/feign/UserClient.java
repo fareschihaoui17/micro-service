4ca4a2703db4e730372d6c48250dc3ed842d39bc
fareschihaoui17 <fares.chihaoui@esprit.tn>
2024-07-22 21:21:17 +0100

    fix circuit breaker

--- product-services/src/main/java/com/microservice/productservices/feign/UserClient.java
+++ product-services/src/main/java/com/microservice/productservices/feign/UserClient.java
@@ -2,6 +2,7 @@ package com.microservice.productservices.feign;
 
 import com.microservice.productservices.dto.UserDTO;
 import com.microservice.productservices.exception.ProjectException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface UserClient {
 
     @GetMapping("/api/users/{id}")
+    @CircuitBreaker(name = "userClient", fallbackMethod = "getUserByIdFallback")
     UserDTO getUserById(@PathVariable Long id) throws ProjectException;
+
+
+    default UserDTO getUserByIdFallback(Long id, Throwable throwable) {
+        return UserDTO.builder()
+                .id(id)
+                .firstName("fake firstName")
+                .lastName("fake lastName")
+                .email("fake@esprit.tn")
+                .password("fake password")
+                .username("fake username")
+                .build();
+    }
 
 }
