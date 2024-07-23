dd8852345bef89e53c77f97311ddb389ea84cd1b
fareschihaoui17 <fares.chihaoui@esprit.tn>
2024-07-23 09:20:29 +0100

    add kafka

--- user-service/src/main/java/com/microservice/userservice/service/ProductConsumer.java
+++ user-service/src/main/java/com/microservice/userservice/service/ProductConsumer.java
@@ -1,10 +1,7 @@
 package com.microservice.userservice.service;
 
-import com.microservice.userservice.dto.ProductAddResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
